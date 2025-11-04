1f6ac88602018857619b92444697293c0d9bd039
ThinhNguyenrt <171249327+ThinhNguyenrt@users.noreply.github.com>
2025-11-04 21:15:43 +0700

    setting up project

---
+++ client-app/src/api/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api', // Proxied
+});
+
+// Auto-attach JWT to every request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const loginUser = async (credentials) => {
+  const response = await api.post('/login', credentials);
+  return response.data;
+};
+
+export const getProfile = async () => {
+  const response = await api.get('/users/me');
+  return response.data;
+};
+
+export const getUsers = async () => {
+  const response = await api.get('/users');
+  return response.data;
+};
+
+export const createUser = async (userData) => {
+  const response = await api.post('/users', userData);
+  return response.data;
+};
+
+export const updateUser = async (id, userData) => {
+  const response = await api.put(`/users/${id}`, userData);
+  return response.data;
+};
+
+export const deleteUser = async (id) => {
+  await api.delete(`/users/${id}`);
+};
+
+// Add more: categories, news, reports, etc.
+export default api;
